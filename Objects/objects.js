fcca45b3be7d203c8109d97dabf5f56662c66b54
ypushiev <pyshievgeeks@gmail.com>
2020-01-17 22:41:36 +0200

    object modifiaed

--- Objects/objects.js
+++ Objects/objects.js
@@ -129,3 +129,107 @@ console.log(a === b); // true
 let a = {};
 let b = {};
 console.log(a == b); // false
+
+// CONST OBJECTS
+// OBJECT which was announced like const - can be changed
+
+const user = {
+	name: "John"
+};
+
+user.age = 25;
+console.log(user.age);
+
+// this variant call error
+
+user = {
+	name: "Peter" // it will error
+}
+
+// CLONE AND MERGE OBJECTS
+
+const user = {
+	name: "John",
+	surname: "Petrenko"
+};
+
+let clone = {}; // empty object
+
+for(let key in user)
+{
+	clone[key]= user[key]; // clone object in
+}
+
+clone.name = "Petro"; // change data
+
+console.log(clone.name);
+
+// CLONE WITH METHOD Object.assign()
+
+let user = { name: "John" };
+
+let permissions1 = { canView: true };
+let permissions2 = { canEdit: true };
+
+// копируем все свойства из permissions1 и permissions2 в user
+Object.assign(user, permissions1, permissions2);
+
+// now user = { name: "John", canView: true, canEdit: true }
+
+// SIMPLE CLONE
+
+let user = {
+  name: "John",
+  age: 30
+};
+
+let clone = Object.assign({}, user);
+
+// IF OBJECT HAS OBJECT
+
+let user = {
+  name: "John",
+  sizes: {
+    height: 182,
+    width: 50
+  }
+};
+
+let clone = Object.assign({}, user);
+
+console.log( user.sizes === clone.sizes ); // true, один и тот же объект
+
+// user и clone обращаются к одному sizes
+user.sizes.width++;       // меняем свойство в одном объекте
+console.log(clone.sizes.width); // 51, видим результат в другом объекте 
+
+
+// TASKS 
+
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name="Peter";
+delete user.name;
+
+
+// task 2
+
+// let obj = {}
+
+function isEmpty(obj){
+
+	let bool = true;
+	for(let key in obj)
+	{
+		bool = false;
+		return bool;
+	}
+	return bool;
+
+}
+
+// task 3
+
+
+
