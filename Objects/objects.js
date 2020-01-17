3cb5c7d3b17bf66597fc59a34ede3dc6d66547d3
Kobzarko <evgewi4@gmail.com>
2020-01-17 15:06:45 +0200

    objects add

--- Objects/objects.js
+++ Objects/objects.js
@@ -26,7 +26,7 @@
 let user = {};
 
 // присваивание значений свойству
-user['likes birds']=true;
+user['likes birds'] = true;
 
 // получение значений свойства
 alert(user['likes birds']); // true
@@ -40,13 +40,13 @@ let options = {
 	width: 1024,
 	height: 768,
 	name: "text", // висячая запятая последняя запятая можно оставить
-	
+
 };
 
 console.log(options.name);
 options.bool = false;
 
-options.colors ={
+options.colors = {
 	border: "black",
 	background: "red"
 };
@@ -59,9 +59,73 @@ console.log(options);
 
 // цикл for in
 // вывод свойств и их значений
-for(let key in options ) {
-	console.log('property ' + key + ' has ' + options[key]);
+for (let key in options) {
+	console.log('property ' + key + ' has ' + options[key]); // property width has 1024
+}
+// or this
+console.log(Object.keys(options).length);
+
+// проверка существования свойства
+// при отсутствии свойства код выдаст undefined
+let person = {};
+console.log(person.someProp === undefined); // true 
+// если свойство изначально undefined  то используем 'in'
+
+// special operator 'in' for check property
+let person = {
+	name: "John",
+	surname: "Ivanov"
+}
+console.log("name" in person, " name is exist"); // true
+console.log("age" in person, " age is not exist"); // false
+
+// ORDER IN PROPERTIES
+
+let codes = {
+	"49": "Germany",
+	"41": "Swisstzarland",
+	"44": "Great Britain",
+	"1": "USA"
 }
 
+for (let code in codes) {
+	console.log(code);
+} // 1, 41, 44, 49
 
-console.log(Object.keys(options).length);
+// but if add '+' to number it will be order by create
+// because it's not a number 
+
+let codes = {
+	"+49": "Germany",
+	"+41": "Swisstzarland",
+	"+44": "Great Britain",
+	"+1": "USA"
+}
+
+for (let code in codes) {
+	console.log(code);
+}
+
+// COPY BY REFERENCE
+
+let user = {
+	name: "Ivasyk"
+}
+let admin = user;
+admin.name = 'Peter'; // changed property by variable "admin"
+console.log(user.name); // 'Peter' changes we can see from variable 'user'
+
+// COMPARING OBJECTS
+
+// '==' and '==='  are equal
+
+let a = {};
+let b = a;
+console.log(a == b); // true
+console.log(a === b); // true
+
+// in this variant
+
+let a = {};
+let b = {};
+console.log(a == b); // false
