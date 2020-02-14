3bee3394a76a111f2a1056df7c6844156e60d785
ypushiev <pyshievgeeks@gmail.com>
2020-02-14 23:53:46 +0200

    add modal Window

---
+++ Modal Window/wind.js
@@ -0,0 +1,33 @@
+
+// Modal 
+
+window.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+
+
+	 // our variables
+	let more = document.querySelector('.more'),
+		overlay = document.querySelector('.overlay'),
+		close = document.querySelector('.popup-close');
+		// create our window
+	more.addEventListener('click', ()=>{
+		overlay.style.display = 'block';   // window has appear style after click
+		this.classList.add('more-splash'); //add class animation for this button 
+		document.body.style.overflow = 'hidden'; // stop scroll site page
+	});
+
+	// close our window
+	close.addEventListener('click', ()=>{
+		overlay.style.display = 'none';    // button has block style after click
+		more.classList.remove('more-splash'); // remove classList animation for this button 
+		document.body.style.overflow = '';  // can scroll page
+	});
+
+
+	// if you want to cancel to scroll page add this code row
+
+	
+
+
+
+});
