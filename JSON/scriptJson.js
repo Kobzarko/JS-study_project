857e400b99d7dd4a9a5ec040810da2e1e0ea4a6f
ypushiev <pyshievgeeks@gmail.com>
2020-02-19 23:39:05 +0200

    add JSON

---
+++ JSON/scriptJson.js
@@ -0,0 +1,68 @@
+// let options = {
+// 	width: 200,
+// 	height: 100,
+// 	background: 'green',
+// 	font: {
+// 		size: '15px',
+// 		color: '#fff'
+// 	}
+// };
+
+
+// // -------JSON.stringify----------------------------------------
+
+// // {"width":200,"height":100,"background":"green","font":{"size":"15px","color":"#fff"}}
+// let json =JSON.stringify(options);
+// console.log(json);
+// console.log( `тип данных  ${typeof(json)}`);
+
+// //  JSON.parse для преобразования JSON обратно в объект.--------------------
+// console.log(JSON.parse(json));
+// let val = JSON.parse(json);
+// // тип данных object
+// console.log( `тип данных  ${typeof(val)}`);
+
+
+// -------------How To Convert FormData To JSON Object----------------------
+
+// const formData = new FormData(SomeFormElement);
+// let jsonObject = {};
+
+// for (const [key, value]  of formData.entries()) {
+   
+//     console.log( jsonObject[key] = value);
+// }
+
+
+//---------- converter-----------------JSON-----------------
+
+let inputUah = document.getElementById('uah');
+let inputUsd = document.getElementById('usd');
+
+inputUah.addEventListener('input', ()=> {
+	let request = new XMLHttpRequest();
+	//requst.open(method, url, async, login , pass);
+	request.open('GET', 'current.json');
+	// настройка запроса
+	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	// отправка запуск запроса
+	request.send();
+
+	// status  - server 
+	// statusText  - text answer
+	// responseText/response  - то что передается ответ от сервера
+	// readyState - текущее состояние запроса
+
+	// отслеживаем состояние сервера и реагируем на состояние 
+	request.addEventListener('readystatechange', function(){
+		if(request.readyState === 4 && request.status == 200 ){
+			let data = JSON.parse(request.response);
+				// получаем курс валют
+			inputUsd.value = inputUah.value/ data.usd;
+		}else{
+			inputUsd.value = "Error server";
+		}
+
+	})
+
+})
